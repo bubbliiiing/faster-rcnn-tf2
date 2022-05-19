9bedb4591fbb447b7828f8a6111735ea8b29d869
Bubbliiiing <3323290568@qq.com>
2022-05-19 22:00:54 +0800

    update eval when train

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -140,7 +140,7 @@ class EvalCallback(keras.callbacks.Callback):
         #-------------------------------------------------------------#
         #   利用建议框获得classifier网络预测结果
         #-------------------------------------------------------------#
-        classifier_pred = self.model_classifier([rpn_pred[2], rpn_results[:, :, [1, 0, 3, 2]]])
+        classifier_pred = self.model_all([image_data, rpn_results[:, :, [1, 0, 3, 2]]])[-2:]
         classifier_pred = [x.numpy() for x in classifier_pred]
         #-------------------------------------------------------------#
         #   利用classifier的预测结果对建议框进行解码，获得预测框
